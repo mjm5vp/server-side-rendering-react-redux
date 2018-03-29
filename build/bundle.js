88179774e218839a8f6268f7ce68bc257ffb1550
Mark Moeller <MarkMoellerUVA@gmail.com>
2018-03-28 22:40:24 -0400

    server side react added

--- build/bundle.js
+++ build/bundle.js
@@ -7055,20 +7055,36 @@ module.exports = memoizeStringOnly;
 "use strict";
 
 
-var express = __webpack_require__(53);
-var React = __webpack_require__(21);
-var renderToString = __webpack_require__(109).renderToString;
-var Home = __webpack_require__(118).default;
-var app = express();
+var _express = __webpack_require__(53);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(21);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(109);
+
+var _Home = __webpack_require__(118);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.use(_express2.default.static('public'));
 
 app.get('/', function (req, res) {
-	var content = renderToString(React.createElement(Home, null));
+  var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
-	res.send(content);
+  var html = '\n    <html>\n      <head></head>\n      <body>\n        <div id="root">' + content + '</div>\n        <script src="bundle.js"></script>\n      </body>\n    </html>\n  ';
+
+  res.send(html);
 });
 
 app.listen(3000, function () {
-	console.log('listening on port 3000');
+  console.log('listening on port 3000');
 });
 
 /***/ }),
@@ -22686,7 +22702,20 @@ var Home = function Home() {
 	return _react2.default.createElement(
 		'div',
 		null,
-		'I\'m the Home component'
+		_react2.default.createElement(
+			'div',
+			null,
+			'I\'m the Home component'
+		),
+		_react2.default.createElement(
+			'button',
+			{
+				onClick: function onClick() {
+					console.log('oh hai Mark');
+				}
+			},
+			'Press me'
+		)
 	);
 };
 
--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Home = () => {
-	return <div>I'm the Home component</div>;
+	return (
+		<div>
+			<div>I'm the Home component</div>
+			<button
+				onClick={() => {
+					console.log('oh hai Mark');
+				}}
+			>
+				Press me
+			</button>
+		</div>
+	);
 };
 
 export default Home;
